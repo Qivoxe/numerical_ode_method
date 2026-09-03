7376a1699e0b00339768a6ba73b78ed5e3a4892e
unknown <shivamthechad@gmail.com>
2026-09-03 19:42:48 +0530

    implemented absolute error and euler approximation

--- src/euler.py
+++ src/euler.py
@@ -9,11 +9,9 @@ def euler(f,x0,y0,h,n):
     x = x0
     y = y0
 
-
     for i in range(n):
         y = y + h * f(x,y)
         x = x + h
-
 
         x_values.append(x)
         y_values.append(y)
@@ -24,12 +22,21 @@ def euler(f,x0,y0,h,n):
 
 
 def f(x, y):
-    return y 
+    return y
 
-x, y = euler(f, 0, 1, 0.1, 10) 
-print("x =", x) 
-print("y =", y) 
-print("Euler approximation at x=1:", y[-1]) 
-print("Exact value:", math.e) 
-print("Absolute error:", abs(math.e - y[-1]))    
 
+step_sizes = [0.1, 0.05, 0.025, 0.0125]
+
+for h in step_sizes:
+
+    n = int(1 / h)
+
+    x, y = euler(f, 0, 1, h, n)
+
+    approximation = y[-1]
+    error = abs(math.e - approximation)
+
+    print(f"h = {h}")
+    print(f"Euler_approximation = {approximation}")
+    print(f"Absolute_error = {error}")
+    print()
